c067855d40dd159350bf0e37d6b41f51091f3838
Fatiha26 <f6lamia@gmail.com>
2024-11-17 16:28:30 +0600

    routing modified

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -67,37 +67,43 @@ const Navbar = () => {
         <div className="lg:hidden absolute top-10 right-4 px-16 py-2  bg-white shadow-md rounded-lg">
           <div className="flex flex-col items-center space-y-4 py-4">
             <Link
-              onClick={toggleMenu}
-              className="text-secondary-400 hover:text-black"
+              className="px-4 py-2 border border-white text-secondary-400 hover:text-black hover:border-secondary-200 rounded-lg"
               href="/"
+            >
+              Home
+            </Link>
+            <Link
+              onClick={toggleMenu}
+              className="px-4 py-2 border border-white text-secondary-400 hover:text-black hover:border-secondary-200 rounded-lg"
+              href="#about"
             >
               About
             </Link>
             <Link
               onClick={toggleMenu}
-              className="text-secondary-400 hover:text-black"
-              href="/"
+              className="px-4 py-2 border border-white text-secondary-400 hover:text-black hover:border-secondary-200 rounded-lg"
+              href="#blog"
             >
               Blogs
             </Link>
             <Link
               onClick={toggleMenu}
-              className="text-secondary-400 hover:text-black"
-              href="/"
+              className="px-4 py-2 border border-white text-secondary-400 hover:text-black hover:border-secondary-200 rounded-lg"
+              href="#skill"
             >
               Skills
             </Link>
             <Link
               onClick={toggleMenu}
-              className="text-secondary-400 hover:text-black"
-              href="/"
+              className="px-4 py-2 border border-white text-secondary-400 hover:text-black hover:border-secondary-200 rounded-lg"
+              href="#project"
             >
               Projects
             </Link>
             <Link
               onClick={toggleMenu}
-              className="text-secondary-400 hover:text-black"
-              href="/"
+              className="px-4 py-2 border border-white text-secondary-400 hover:text-black hover:border-secondary-200 rounded-lg"
+              href="#contact"
             >
               Contact
             </Link>
